bef90469a35bef7423118258ef455beaf6d65b69
Toufik Airane <toufik.airane@gmail.com>
2015-10-28 12:11:04 +0100

    Update start.py

--- Sulley/start.py
+++ Sulley/start.py
@@ -10,6 +10,9 @@ with open("fuzz.flow.udg", "w+") as hf:
 
 # Scenario
 sess.connect(s_get("restore"))
+sess.connect(s_get("set"), s_get("append"))
+sess.connect(s_get("rpush"), s_get("lpush"))
+sess.connect(s_get("lpush"), s_get("rpush"))
 
 target = sessions.target("192.168.109.138", 6379)
 sess.add_target(target)
